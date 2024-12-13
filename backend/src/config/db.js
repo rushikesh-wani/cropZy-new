fee2b75514ec90505626e2d0451adadeedb77a67
Rushikesh Wani <wanirushikeshanil11@gmail.com>
2024-12-13 12:45:18 +0530

    mongo str added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  console.log(process.env.MONGO_URI);
   mongoose.set("debug", true);
   const options = {
     dbName: "cropZy",
   };
-  await mongoose.connect(process.env.MONGO_URI, options);
+  await mongoose.createConnection(
+    "mongodb+srv://rushi11:rushi11@cluster0.1ubgl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    options
+  );
 };
 
 module.exports = { connectDB };
