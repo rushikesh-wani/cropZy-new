e8513c8df96feaa32eb57095fcfb8f3231f340d4
Rushikesh Wani <wanirushikeshanil11@gmail.com>
2024-12-13 13:05:47 +0530

    modified db connect file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  console.log(process.env.MONGO_URI);
   mongoose.set("debug", true);
   const options = {
     dbName: "cropZy",
   };
-  await mongoose.createConnection(
-    "mongodb+srv://rushi11:rushi11@cluster0.1ubgl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    options
-  );
+  await mongoose.connect(process.env.MONGO_URI, options);
 };
 
 module.exports = { connectDB };
